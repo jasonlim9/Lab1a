0de8e4e605252adef0170019bee454d4a1f640a6
jasonlim9 <33109108+jasonlim9@users.noreply.github.com>
2017-11-25 20:48:09 +0800

    Add files via upload

---
+++ delete_data.php
@@ -0,0 +1,68 @@
+<link href="colour.css" rel="stylesheet">
+<html>
+<body>
+<b><center>PRODUCTS</center></b>
+<?php
+$con = mysqli_connect("localhost","root","","swap"); //connect to database
+if (!$con){
+	die('Could not connect: ' . mysqli_connect_errno()); //return error is connect fail
+}
+?>
+
+<?php
+$id=$_POST['idnum'];
+$deleteprod=$con->prepare("DELETE FROM products WHERE productID='". $id ."'");
+$deleteprod->execute();
+
+
+if ($deleteprod->execute())
+{  										//execute query
+  echo "<table align='center'>";
+  echo "<tr>";
+  echo "<th>Product successfully removed.</th>";
+  echo "</tr>";
+}
+else
+{
+  echo "<table align='center'>";
+  echo "<tr>";
+  echo "<th>There is no such item, Please re-enter again.</th>";
+  echo "</tr>";
+}
+$showprod=$con->prepare("select * from products ");
+$showprod->execute();
+$showprod->bind_result($productID,$productName,$price,$size,$colour,$category,$quantity);
+echo "<table align='center' border='1'>";
+echo "<tr>";
+echo "<th>Product ID</th>";
+echo "<th>Product Name</th>";
+echo "<th>Price</th>";
+echo "<th>Size</th>";
+echo "<th>Colour</th>";
+echo "<th>Category</th>";
+echo "<th>Quantity</th>";
+echo "</tr>";
+while($showprod->fetch())
+{
+	echo "<tr>";
+	echo "<td>".$productID."</td>";
+	echo "<td>".$productName."</td>";
+	echo "<td>".$price."</td>";
+	echo "<td>".$size."</td>";
+	echo "<td>".$colour."</td>";
+	echo "<td>".$category."</td>";
+	echo "<td>".$quantity."</td>";
+	echo "</tr>";	
+	
+}
+echo "</table>";
+?>
+
+<br>
+<table align='center'>
+<tr>
+<td> <a href="display_all.php"><button class="button" style="vertical-align:middle"><span>Back to home</span></button></td>
+</tr></table>
+
+</body>
+</html>
---
+++ insert_data.php
@@ -0,0 +1,73 @@
+<link href="colour.css" rel="stylesheet">
+<html>
+<body>  
+
+<?php
+$con = mysqli_connect("localhost","root","","swap"); //connect to database
+if (!$con){
+	die('Could not connect: ' . mysqli_connect_errno()); //return error is connect fail
+}
+$id = $_POST['prodid'];
+$name = $_POST['prodname'];
+$price = $_POST['prodprice'];
+$size = $_POST['prodsize'];
+$colour = $_POST['prodcolour'];
+$cat = $_POST['prodcat'];
+$quan = $_POST['prodquan'];
+
+$insertdata= $con->prepare("INSERT INTO products (`productID`,`productName`,`price`,`size`,`colour`,`category`,`quantity`) VALUES
+(?,?,?,?,?,?,?)");
+
+$insertdata->bind_param('sssssss',$id,$name,$price,$size,$colour,$cat,$quan); //bind the parameters
+if ($insertdata->execute())
+{  										//execute query
+  echo "<table align='center'>";
+  echo "<tr>";
+  echo "<th>Product successfully added.</th>";
+  echo "</tr>";
+}
+else
+{
+  echo "<table align='center'>";
+  echo "<tr>";
+  echo "<th>Items existed, Please re-enter again.</th>";
+  echo "</tr>";
+}
+
+
+$showprod=$con->prepare("select productID,productName,price,size,colour,category,quantity from products ");
+$showprod->execute();
+$showprod->bind_result($productID,$productName,$price,$size,$colour,$category,$quantity);
+echo "<table align='center' border='1'>";
+echo "<tr>";
+echo "<th>Product ID</th>";
+echo "<th>Product Name</th>";
+echo "<th>Price</th>";
+echo "<th>Size</th>";
+echo "<th>Colour</th>";
+echo "<th>Category</th>";
+echo "<th>Quantity</th>";
+echo "</tr>";
+while($showprod->fetch())
+{
+	echo "<tr>";
+	echo "<td>".$productID."</td>";
+	echo "<td>".$productName."</td>";
+	echo "<td>".$price."</td>";
+	echo "<td>".$size."</td>";
+	echo "<td>".$colour."</td>";
+	echo "<td>".$category."</td>";
+	echo "<td>".$quantity."</td>";
+	echo "</tr>";	
+	
+}
+echo "</table>";
+?>
+
+<table align='center'>
+<br><tr>
+<td> <a href="display_all.php"><button class="button" style="vertical-align:middle"><span>Back to home</span></button></td>
+</tr></table>
+
+</body>
+</html>
